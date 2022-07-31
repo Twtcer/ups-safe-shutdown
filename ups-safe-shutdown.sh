9bc5c944da7cfe812d6adaf212489a206bb3ad92
LandWind <LandWind1180@gmail.com>
2022-07-31 18:59:45 +0800

    add pushplus msg

--- ups-safe-shutdown.sh
+++ ups-safe-shutdown.sh
@@ -15,8 +15,13 @@ do
   sleep 10s
   if [ $failure_count -eq $shutdown_failure_count_threshold ]; then
     /sbin/shutdown -hP now
+	sendMsg
     break
   fi
 done
+
+sendMsg(){
+	curl -H "Content-Type: application/json" -X POST -d '{"topic":"pve","token":"e5145cdd4e4442d29e7d9dc0944ca94c","title":"断电提醒","content":"## 时间： \r\n## 断电了1111","template":"markdown","channel":"wechat"}' "https://www.pushplus.plus/api/send"
+}
 
 exit 0
